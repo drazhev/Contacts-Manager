8c75c32fadc54af5049c0fdd849dc9209565d081
Alexandar Drajev <a.drajev@MMAdmins-Mac-mini-98.local>
2013-12-10 18:31:40 +0200

    Create the project as tabbed application

---
+++ 5-Home-Contacts/SecondViewController.h
@@ -0,0 +1,13 @@
+//
+//  SecondViewController.h
+//  5-Home-Contacts
+//
+//  Created by Alexandar Drajev on 12/10/13.
+//  Copyright (c) 2013 Alexander Drazhev. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SecondViewController : UIViewController
+
+@end
---
+++ 5-Home-Contacts/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  5-Home-Contacts
+//
+//  Created by Alexandar Drajev on 12/10/13.
+//  Copyright (c) 2013 Alexander Drazhev. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
